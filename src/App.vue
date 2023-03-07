d7c3ad716f8d650bde554449157ce2e11da287e9
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2023-03-07 15:56:05 -0800

    setup ionic framework plus vuejs

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script>
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+export default {
+  components: {
+    IonApp,
+    IonRouterOutlet,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>

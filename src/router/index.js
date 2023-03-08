9c027886786cb9e8a73bcc630d64c5866547f194
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2023-03-07 23:32:40 -0800

    add meta stuffs

--- src/router/index.js
+++ src/router/index.js
@@ -12,22 +12,36 @@ const routes = [
     path: "/home",
     name: "Home",
     component: HomePage,
+    meta: {
+      title: "Home | Ariyibi Baseet",
+    },
   },
   {
     path: "/project-page",
     name: "ProjectPage",
     component: ProjectPage,
+    meta: {
+      title: "Project | Ariyibi Baseet",
+    },
   },
   {
     path: "/contact-page",
     name: "ContactPage",
     component: ContactPage,
+    meta: {
+      title: "Contact | Ariyibi Baseet",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
